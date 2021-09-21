9d43554701cf95942676646d35be153170c976be
Fetisov Vladislav <vladfetisov2011@yandex.ru>
2021-09-21 22:23:16 +0300

    AisBrand feat: add serialization for xelement

--- AisBrand/BrandDataProcessing/Serializated.cs
+++ AisBrand/BrandDataProcessing/Serializated.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
 using System.Xml.Serialization;
 
 namespace BrandDataProcessing
@@ -18,6 +22,22 @@ namespace BrandDataProcessing
             XmlSerializer formatter = new XmlSerializer(typeof(T));
             using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None))
                 return (T)formatter.Deserialize(stream);
+        }
+
+        public static IEnumerable<T> XmlDeserialization(IEnumerable<XElement> elements)
+        {
+            if (elements == null)
+                throw new ArgumentNullException(nameof(elements));
+
+            List<T> objects = new List<T>();
+            XmlSerializer formatter = new XmlSerializer(typeof(T));
+            foreach (XElement obj in elements)
+            {
+                T deserializedObject = (T)formatter.Deserialize(obj.CreateReader());
+                objects.Add(deserializedObject);
+            }
+
+            return objects;
         }
     }
 }
