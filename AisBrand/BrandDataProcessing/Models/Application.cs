4388f4e7fcc9e811da824f42ebe091726199efce
Fetisov Vladislav <vladfetisov2011@yandex.ru>
2021-09-28 19:59:54 +0300

    AisBrand feat: add serialization array of bytes in application

--- AisBrand/BrandDataProcessing/Models/Application.cs
+++ AisBrand/BrandDataProcessing/Models/Application.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Xml.Serialization;
 
 namespace BrandDataProcessing.Models
 {
@@ -7,6 +9,14 @@ namespace BrandDataProcessing.Models
     {
         public int ClassificationID { get; set; }
 
-        public byte[] File { get; set; }
+        [XmlIgnore]
+        public string FileAsString { get; set; }
+
+        [XmlElement(ElementName = "FileAsString", DataType = "hexBinary")]
+        public byte[] File
+        {
+            get { return Encoding.UTF8.GetBytes(FileAsString ?? string.Empty); }
+            set { FileAsString = Encoding.UTF8.GetString(value); }
+        }
     }
 }
