4374fbb3448e4e1ee3d5e85a2e2f00e8c65d318b
Fetisov Vladislav <vladfetisov2011@yandex.ru>
2021-10-12 22:40:50 +0300

    AisBrand feat: add enum for RomanNumerals

---
+++ AisBrand/BrandDataProcessing/RomanNumerals.cs
@@ -0,0 +1,12 @@
+﻿namespace BrandDataProcessing
+{
+    public enum RomanNumerals
+    {
+        M = 1000,
+        D = 500,
+        C = 100,
+        X = 10,
+        V = 5,
+        I = 1
+    }
+}
