2a33b93258ab0ca66322617852f6e2409de0164d
Fetisov Vladislav <vladfetisov2011@yandex.ru>
2021-11-21 18:30:04 +0300

    AisBrand feat: add filters

---
+++ AisBrand/GenericFilterTests/GenericFilterTests.cs
@@ -0,0 +1,34 @@
+﻿using BrandDataProcessing.DAL;
+using BrandDataProcessing.Models;
+using GenericFilters;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericFilterTests
+{
+    [TestFixture]
+    public class GenericFilterTests
+    {
+        private const string fileName = @"D:\Work\AisBrand\AisBrand\BrandDataProcessingTests\bin\Debug\net5.0\Test.xml";
+
+        [Test]
+        public void TestCheckOnEmpty_NullFieldNumberBrand_Success()
+        {
+            const int expectedExcavationsCount = 1;
+            const int expectedExcavationId = 3;
+
+            BrandLocal brandLocal = new BrandLocal(fileName);
+            IEnumerable<Brand> brands = brandLocal.GetWithEmptyName();
+            GenericFilter<Brand> filter = new GenericFilter<Brand>();
+            filter.CheckOnEmpty("FieldNumber");
+            filter.Apply(brands);
+
+            int actualExcavationCount = brands.Count();
+            int actualExcavationId = brands.Single().ID;
+
+            Assert.AreEqual(expectedExcavationsCount, actualExcavationCount);
+            Assert.AreEqual(expectedExcavationId, actualExcavationId);
+        }
+    }
+}
