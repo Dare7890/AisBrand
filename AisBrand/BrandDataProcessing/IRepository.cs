dd86bdb41003934d4bf7033298f686c5d6d58f08
Fetisov Vladislav <vladfetisov2011@yandex.ru>
2021-11-21 18:31:09 +0300

    AisBrand feat: add IRepository

---
+++ AisBrand/BrandDataProcessing/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BrandDataProcessing
+{
+    public interface IRepository<T>
+    {
+        void Add(T item);
+        void Update(T item);
+        void Delete(int id);
+        IEnumerable<T> GetAll();
+    }
+}
