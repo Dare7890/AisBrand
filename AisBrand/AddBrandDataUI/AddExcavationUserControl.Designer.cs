a0261f547b656c5315ad5cd8a47274c0366bfdc2
Fetisov Vladislav <vladfetisov2011@yandex.ru>
2021-12-07 21:58:03 +0300

    AisBrand feat: add user control for input excavation data

---
+++ AisBrand/AddBrandDataUI/AddExcavationUserControl.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace AddBrandDataUI
+{
+    partial class AddExcavationUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblMonument = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtMonument = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(13, 23);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(62, 15);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Название:";
+            // 
+            // lblMonument
+            // 
+            this.lblMonument.AutoSize = true;
+            this.lblMonument.Location = new System.Drawing.Point(13, 56);
+            this.lblMonument.Name = "lblMonument";
+            this.lblMonument.Size = new System.Drawing.Size(65, 15);
+            this.lblMonument.TabIndex = 1;
+            this.lblMonument.Text = "Памятник:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(81, 20);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(175, 23);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtMonument
+            // 
+            this.txtMonument.Location = new System.Drawing.Point(81, 53);
+            this.txtMonument.Name = "txtMonument";
+            this.txtMonument.Size = new System.Drawing.Size(175, 23);
+            this.txtMonument.TabIndex = 3;
+            // 
+            // addExcavation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtMonument);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblMonument);
+            this.Controls.Add(this.lblName);
+            this.Name = "addExcavation";
+            this.Size = new System.Drawing.Size(270, 97);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblMonument;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtMonument;
+    }
+}
---
+++ AisBrand/AddBrandDataUI/AddExcavationUserControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+using AddBrandDataUI.ViewModels;
+
+namespace AddBrandDataUI
+{
+    public partial class AddExcavationUserControl : UserControl
+    {
+        public AddExcavationUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public Excavation Add()
+        {
+            string name = txtName.Text.Trim();
+            string monument = txtMonument.Text.Trim();
+
+            return new Excavation(name, monument);
+        }
+    }
+}
