039277659ac075351e0b52c36b5cf055726ba118
Fetisov Vladislav <vladfetisov2011@yandex.ru>
2021-12-07 21:56:53 +0300

    AisBrand feat: add excavation ViewModel

---
+++ AisBrand/AddBrandDataUI/ViewModels/Excavation.cs
@@ -0,0 +1,15 @@
+﻿namespace AddBrandDataUI.ViewModels
+{
+    public class Excavation
+    {
+        public string Name { get; }
+
+        public string Monument { get; }
+
+        public Excavation(string name, string monument)
+        {
+            Name = name;
+            Monument = monument;
+        }
+    }
+}
